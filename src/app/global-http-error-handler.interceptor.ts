706a9fe8f038b1808bb11d86a096f32ec02a058a
fabossif <fabossif@gmail.com>
2024-11-24 11:55:55 -0300

    Error handlers implemented

---
+++ src/app/global-http-error-handler.interceptor.ts
@@ -0,0 +1,32 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, retry, throwError, timer } from 'rxjs';
+
+@Injectable()
+export class GlobalHttpErrorHandler implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      retry({
+        count: 3,
+        delay: (_, retryCount) => timer(retryCount * 1000),
+      }),
+      catchError((err) => {
+        console.log('Error handled by HTTP interceptor...');
+        return throwError(() => {
+          console.log('Error rethrown by HTTP interceptor');
+          return err;
+        });
+      })
+    );
+  }
+}
